5b912bef2f2ce0d63df7f6d5a8d64b5cdac8600f
dazanonr <dazanonr@indra.es>
2023-11-21 17:54:20 +0100

    3 hilos

--- src/main/java/dazanonr/Main.java
+++ src/main/java/dazanonr/Main.java
@@ -1,22 +1,23 @@
 package dazanonr;
 public class Main
 {
-
     public static void main (String [] args) throws InterruptedException {
 
         CityData cityData = new CityData();
 
-        Thread mainThread = new Thread(new GetInfo(cityData));
-        Thread secondThread= new Thread(new CreateDirectory());
-        Thread thirthThread = new Thread(new WriteDocument(cityData));
+        for (int i = 0; i< 3; i++){
+            Thread mainThread = new Thread(new GetInfo(cityData));
+            Thread secondThread = new Thread(new CreateDirectory());
+            Thread thirthThread = new Thread(new WriteDocument(cityData));
 
-        mainThread.start();
-        mainThread.join();
+            mainThread.start();
+            mainThread.join();
 
-        secondThread.start();
-        secondThread.join();
+            secondThread.start();
+            secondThread.join();
 
-        thirthThread.start();
-        thirthThread.join();
+            thirthThread.start();
+            thirthThread.join();
+        }
     }
 }
--- src/main/java/dazanonr/WriteDocument.java
+++ src/main/java/dazanonr/WriteDocument.java
@@ -13,10 +13,10 @@ public class WriteDocument implements Runnable{
     public void run() {
         try {
             File file = new File("C:\\Users\\dazanonr\\Documents\\CityData\\city_data.txt");
-            Writer writer = new FileWriter(file);
+            Writer writer = new FileWriter(file,true);
             BufferedWriter bufferedWriter = new BufferedWriter(writer);
-            bufferedWriter.newLine();
             System.out.println(cityData.toString());
+            bufferedWriter.newLine();
             bufferedWriter.write(cityData.toString());
         } catch (IOException e) {
             throw new RuntimeException(e);
